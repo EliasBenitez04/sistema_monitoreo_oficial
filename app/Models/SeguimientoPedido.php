2ef5538df86f9e0742446d3c1fd19c8b8b0cbbd1
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-23 15:06:07 -0300

    Crear modulo seguimiento de pedidos

---
+++ app/Models/SeguimientoPedido.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SeguimientoPedido extends Model
+{
+    protected $table = 'seguimiento_pedido';
+    protected $fillable = ['nro_pedido'];
+
+    public function detalles()
+    {
+        return $this->hasMany(SeguimientoPedidoDetalle::class, 'seguimiento_pedido_id');
+    }
+}
