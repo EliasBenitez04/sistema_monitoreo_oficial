67049276d619e7cab57b5b81ee2a8336da3f47a9
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-24 15:30:15 -0300

    Habilitar fecha de pedido en seguimiento

--- app/Models/SeguimientoPedido.php
+++ app/Models/SeguimientoPedido.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class SeguimientoPedido extends Model
 {
     protected $table = 'seguimiento_pedido';
-    protected $fillable = ['nro_pedido'];
+    protected $fillable = ['nro_pedido', 'fecha_pedido'];
+
+    protected $casts = [
+        'fecha_pedido' => 'date',
+    ];
 
     public function detalles()
     {
